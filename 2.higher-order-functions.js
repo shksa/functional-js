02fcaf93855e71c6c99ec6bc6bad8543b3fd1812
sreekar <sreekar339@gmail.com>
2018-01-16 10:15:51 +0530

    first case passed

--- 2.higher-order-functions.js
+++ 2.higher-order-functions.js
@@ -1,5 +1,23 @@
+function count_calls(fn){
+    function wrapper(a, b){
+        wrapper.count += 1
+        let result = fn(a, b)
+        return result
+    }
+    wrapper.count = 0
+    return wrapper
+}
+function dummy(){
+  return 1
+}
+dummy = count_calls(dummy)
+
 function repeat(operation, num) {
       // SOLUTION GOES HERE
+      for(let i=0;i<num;i++){
+        operation()
+      }
+      return operation.count
 }
 // module.exports = repeat
-console.log('should work ', repeat(isNaN, 4) === 4)
+console.log('should work ', repeat(dummy, 4) === 4)
